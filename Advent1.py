8d9481402bc2ac8e6720ca99394e037812452afd
Ardenrian <Ardenrian@users.noreply.github.com>
2024-12-07 12:47:01 +0100

    day1b

--- Advent1.py
+++ Advent1.py
@@ -6,11 +6,15 @@ def ReadFile(fname):
     b = [data[1] for data in my_data]
     return a, b
 
-def CalcDist(a, b):
+def CalcDist(a, b): #1a
     a.sort()
     b.sort()
-    subArray = [a1 - b1 for (a1, b1)in zip(a, b)]
-    return sum (val for val in [abs(val) for val in subArray])
+    substArray = [a1 - b1 for (a1, b1)in zip(a, b)]
+    return sum (val for val in [abs(val) for val in substArray])
+
+def CalcSimilarity(a,b): #1b
+    similArray = [val if val in a else 0 for val in b]
+    return sum(val for val in similArray)
 
 a, b =  ReadFile("Advent1input.txt")
-print(CalcDist(a, b))
+print(CalcSimilarity(a, b))
